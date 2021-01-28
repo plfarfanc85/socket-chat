c1187c93f7c82a60b898ec0ead26f110c7f017b6
plfarfanc85 <plfarfanc@gmail.com>
2021-01-28 18:49:39 -0500

    Chat finalizado

--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -19,13 +19,20 @@ io.on("connection", (client) => {
     let personas = usuarios.getPersonasPorSala(data.sala);
     console.log(personas);
     client.broadcast.to(data.sala).emit("listaPersona", personas);
+    client.broadcast
+      .to(data.sala)
+      .emit(
+        "crearMensaje",
+        crearMensaje("Administrador", `${data.nombre} se unió`)
+      );
     callback(personas);
   });
 
-  client.on("crearMensaje", (data) => {
+  client.on("crearMensaje", (data, callback) => {
     let persona = usuarios.getPersona(client.id);
     let mensaje = crearMensaje(persona.nombre, data.mensaje);
     client.broadcast.to(persona.sala).emit("crearMensaje", mensaje);
+    callback(mensaje);
   });
 
   client.on("disconnect", () => {
